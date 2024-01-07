3c8b41919075afcbc2b5403428f6f1293c3524b0
Charlie <charlie@Charlies-MBP.broadband>
2024-01-07 20:24:51 +0000

    black format

--- tests/test_mortgagepy.py
+++ tests/test_mortgagepy.py
@@ -7,15 +7,30 @@ def test_monthly_interest():
     october_balance = -98_868.70
     september_balance = -99_276.93
 
-    assert mortgagepy.monthly_interest(september_balance, 1.89, 9, 2023) == october_debit_interest
-    assert mortgagepy.monthly_interest(october_balance, 1.89, 10, 2023) == november_debit_interest
+    assert (
+        mortgagepy.monthly_interest(september_balance, 1.89, 9, 2023)
+        == october_debit_interest
+    )
+    assert (
+        mortgagepy.monthly_interest(october_balance, 1.89, 10, 2023)
+        == november_debit_interest
+    )
+
 
 def test_ltv_calculator():
     assert mortgagepy.ltv_calculator(200_000, 50_000) == {"ltv_pct": 75}
 
+
 def test_interest_only_calculator():
-    assert mortgagepy.interest_only_calculator(130_500, 3.89) == {"monthly_repayment": 423.04}
+    assert mortgagepy.interest_only_calculator(130_500, 3.89) == {
+        "monthly_repayment": 423.04
+    }
+
 
 def test_repayment_calculator():
-    assert mortgagepy.repayment_calculator(130_500, 3.89, 300) == {'monthly_repayment': 680.93}
-    assert mortgagepy.repayment_calculator(130_500, 6.89, 300) == {'monthly_repayment': 913.21}
+    assert mortgagepy.repayment_calculator(130_500, 3.89, 300) == {
+        "monthly_repayment": 680.93
+    }
+    assert mortgagepy.repayment_calculator(130_500, 6.89, 300) == {
+        "monthly_repayment": 913.21
+    }
