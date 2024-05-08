e442cf1f8a4d414eca19fefddb2a2047ead52920
Charlie <charlie@Charlies-MBP.broadband>
2024-05-08 20:51:17 +0100

    add tests for comparing interest rates, use fxitures

--- tests/test_mortgagepy.py
+++ tests/test_mortgagepy.py
@@ -1,33 +1,59 @@
-import mortgagepy
+import pytest
+
+from mortgagepy import (
+    IncorrectType,
+    compare_repayment_interest_rates,
+    interest_only_calculator,
+    ltv_calculator,
+    monthly_interest,
+    repayment_calculator,
+    total_cost_of_mortgage,
+)
 
 
-def test_monthly_interest():
+@pytest.fixture
+def mortgage() -> int:
+    return 130_500
+
+
+def test_monthly_interest() -> None:
     november_debit_interest = -158.70
     october_debit_interest = -154.22
     october_balance = -98_868.70
     september_balance = -99_276.93
 
     assert (
-        mortgagepy.monthly_interest(september_balance, 1.89, 9, 2023)
+        monthly_interest(september_balance, 1.89, 9, 2023)
         == october_debit_interest
     )
     assert (
-        mortgagepy.monthly_interest(october_balance, 1.89, 10, 2023)
+        monthly_interest(october_balance, 1.89, 10, 2023)
         == november_debit_interest
     )
 
 
-def test_ltv_calculator():
-    assert mortgagepy.ltv_calculator(200_000, 50_000) == 75
+def test_ltv_calculator(mortgage: int) -> None:
+    assert ltv_calculator(mortgage, 50_000) == 61
 
 
-def test_interest_only_calculator():
-    assert mortgagepy.interest_only_calculator(130_500, 3.89) == 423.04
+def test_interest_only_calculator(mortgage: int) -> None:
+    assert interest_only_calculator(mortgage, 3.89) == 423.04
 
 
-def test_repayment_calculator():
-    assert mortgagepy.repayment_calculator(130_500, 3.89, 300) == 680.93
-    assert mortgagepy.repayment_calculator(130_500, 6.89, 300) == 913.21
+def test_repayment_calculator(mortgage: int) -> None:
+    assert repayment_calculator(mortgage, 3.89, 300) == 680.93
+    assert repayment_calculator(mortgage, 6.89, 300) == 913.21
 
-def test_total_cost_of_mortgage():
-    assert mortgagepy.total_cost_of_mortgage(130_500, 6.89, 300) == 273_963.0
+
+def test_total_cost_of_mortgage(mortgage: int) -> None:
+    assert total_cost_of_mortgage(mortgage, 6.89, 300) == 273_963.0
+
+
+def test_compare_repayment_interest_rates(mortgage: int):
+    with pytest.raises(IncorrectType):
+        compare_repayment_interest_rates(mortgage, 1, 300)
+
+    assert compare_repayment_interest_rates(mortgage, [1, 2], 300) == {
+        1: 491.82,
+        2: 553.13,
+    }
