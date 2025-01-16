648111fc02e9f07f425f36f68c5e1bf0ce8f78ad
Charlie <charlie@Mac.broadband>
2025-01-16 13:37:17 +0000

    add overpayment calculator, ruff linted

--- src/mortgagepy/compare.py
+++ src/mortgagepy/compare.py
@@ -26,10 +26,14 @@ def compare_interest_only_rates(
 
     repayments = dict()
 
-    for interest_rate in interest_rates:
-        repayments[interest_rate] = monthly_interest_only_repayment(
-            mortgage=mortgage, interest_rate=interest_rate
-        )
+    for idx, interest_rate in enumerate(interest_rates):
+        repayments[idx] = {
+            "interest_rate": interest_rate,
+            "repayment": monthly_interest_only_repayment(
+                mortgage=mortgage,
+                interest_rate=interest_rate,
+            ),
+        }
 
     return repayments
 
@@ -56,11 +60,14 @@ def compare_capital_repayment_rates(
 
     repayments = dict()
 
-    for interest_rate in interest_rates:
-        repayments[interest_rate] = monthly_capital_repayment(
-            mortgage=mortgage,
-            interest_rate=interest_rate,
-            mortgage_length_months=mortgage_length_months,
-        )
+    for idx, interest_rate in enumerate(interest_rates):
+        repayments[idx] = {
+            "interest_rate": interest_rate,
+            "repayment": monthly_capital_repayment(
+                mortgage=mortgage,
+                interest_rate=interest_rate,
+                mortgage_length_months=mortgage_length_months,
+            ),
+        }
 
     return repayments
